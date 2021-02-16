a17cf88955d09c0fd97c25c2417716d9652e6e3e
example1 <32572574+bestshine@users.noreply.github.com>
2021-02-16 22:54:45 +0530

    initial commit
    initial commit

---
+++ storage/framework/views/3d79981097893ca98faf63105fd34d9c50456ae8.php
@@ -0,0 +1,21 @@
+<?php $__env->startSection('title', 'Ohhhh'); ?>
+<?php $__env->startSection('content'); ?>
+    <!--404 error-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 text-right u-MarginTop100 u-xs-MarginTop0">
+                <img class="img-fit-responsive " src="assets/imgs/404-icon.png" alt=""/>
+            </div>
+            <div class="col-md-6 col-sm-6 u-MarginTop100 u-xs-MarginTop30 u-xs-MarginBottom30">
+                <div class="u-MarginTop100 u-xs-MarginTop0 u-PaddingLeft50 u-xs-PaddingLeft0">
+                    <img class="img-responsive" src="assets/imgs/404-t.png" alt=""/>
+                    <h1 class="u-weight300">Page Not Found</h1>
+                    <a href="index.html" class="btn btn-default">Back to Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--404 error-->
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.default', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
